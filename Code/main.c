a9aae61bedb578e61bc30ab4ccb356b3bd3171e3
Hamood <elhaissouniaamir@gmail.com>
2025-07-08 23:09:13 -0400

    first commit just tetsing out git a little

--- Code/main.c
+++ Code/main.c
@@ -1,1 +1,20 @@
+// Start of CLI game 
+// Made by Aamir Elhaissouni
+// Started 7-7-25
 
+// Game Goals:
+// Have a game Loop
+// Have different classes
+// Have different stats
+// Have an inbentory
+// Have levels (health, mana, strength etc)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+    //Inital Message/Test
+    printf("Welcome to my CLI_RPG, I hope you have fun!");
+}
